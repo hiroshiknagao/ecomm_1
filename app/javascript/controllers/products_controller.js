569fe8c6836faf5c28b6364983148a6386108548
hiroshi nagao <hi_1205@hotmail.com>
2024-05-20 16:29:28 +0900

    adding functionality to add to cart button but some errors to be fixed

--- app/javascript/controllers/products_controller.js
+++ app/javascript/controllers/products_controller.js
@@ -2,7 +2,43 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="products"
 export default class extends Controller {
-  connect() {
-    console.log("connectd controler")
+  static values= { size: String, product: Object }
+
+  addToCart() {
+    console.log("Prodcut: ", this.productValue)
+    const cart = localStorage.getItem("cart")
+    if (cart) {
+      const cartArray = JSON.parse(cart)
+      const foundIndex = cartArray.findIndex(item => item.id === this.productValue.id && item.size === this.sizeValue)
+      if (foundIndex >= 0) {
+        cartArray[foundIndex].quantity = parseInt(cartArray[foundIndex].quantity)
+        + 1
+      } else {
+        cartArray.push({
+          id: this.productValue.id,
+          name: this.productValue.name,
+          price: this.productValue.price,
+          size: this.sizeValue,
+          quantity: 1
+        })
+
+      }
+    } else {
+      const cartArray = []
+      cartArray.push({
+        id: this.productValue.id,
+        name: this.productValue.name,
+        price: this.productValue.price,
+        size: this.sizeValue,
+        quantity: 1
+      })
+      locaclStorage.setItem("cart", JSON.stringify(cartArray))
+    }
+  }
+
+  selectSize(e) {
+    this.sizeValue = e.target.values
+    const selectedSizeEl = document.getElementById("selected-size")
+    selectedSizeEl.innerText = `Selected Size: ${this.sizeValue}`
   }
 }
