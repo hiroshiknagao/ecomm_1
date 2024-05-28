e5bb49bad9e4d0c4ac36a7bfc1a621ad9f1ae924
hiroshi nagao <hi_1205@hotmail.com>
2024-05-28 16:09:29 +0900

    add stimulus for cart

---
+++ app/javascript/controllers/cart_controller.js
@@ -0,0 +1,34 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="cart"
+export default class extends Controller {
+  initialize() {
+    const cart = JSON.parse(localStorage.getItem("cart"))
+    if (!cart) {
+      return
+    }
+
+    let total = 0
+    for (let i=0; i < cart.length; i++) {
+      const item = cart[i]
+      total += item.price * item.quantity
+      const div = document.createElement("div")
+      div.classList.add("mt-2")
+      div.innerText = `Item: ${item.name} - $${item.price/100.0} - Size:
+        ${item.size} - Quantity: ${item.quantity}`
+        const deleteButton = document.createElement("button")
+        deleteButton.innerText = "Remove"
+        deleteButton.value = item.div
+        deleteButton.classList.add("bg-gray-500", "rounded", "text-white",
+         "px-2", "py-1", "ml-2")
+         deleteButton.addEventListener("click", this.removeFromCart)
+         div.appendChild(deleteButton)
+         this.element.prepend(div)
+        }
+        const totalEl = document.createElement("div")
+        totalEl.innerText = `Total: $${total/100.0}`
+        let totalController = document.getElementById("total")
+        totalController.appendChild(totalEl)
+
+  }
+}
